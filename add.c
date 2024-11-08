0fc893db17d485e16d9a901e1d79ab865a8989e0
shreyarane24 <166697926+shreyarane24@users.noreply.github.com>
2024-11-08 21:39:07 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<iostream>
+Using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a and b:"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition is "<<c;
+    return 0;
+}
